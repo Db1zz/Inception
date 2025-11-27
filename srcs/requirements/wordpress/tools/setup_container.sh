e9eba73082d99cb00ffe5821742911eb839e9599
Georgijs Onischenko <xel27@inbox.lv>
2025-11-27 12:38:18 +0100

    done

--- srcs/requirements/wordpress/tools/setup_container.sh
+++ srcs/requirements/wordpress/tools/setup_container.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+if [ -z "$( ls -A /usr/local/bin/ | grep "wp" )" ]; then
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+fi
+
+if [ -z "$(ls /app -A | grep "index.php")" ]; then
+    wp core download --allow-root
+fi
+
 wp language core install en_US --activate --allow-root
 
 wp core install \

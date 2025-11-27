63dfdcca2380a8b64080f521d6fa3423d6432db7
Georgijs Onischenko <xel27@inbox.lv>
2025-11-27 01:42:28 +0100

    aboba

--- srcs/requirements/wordpress/tools/setup_container.sh
+++ srcs/requirements/wordpress/tools/setup_container.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-
-if [ -z "$( ls -A /usr/local/bin/ | grep "wp" )" ]; then
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
-fi
-
-if [ -z "$(ls /app -A | grep "index.php")" ]; then
-    wp core download --allow-root
-fi
-
-if [ -z "$(find / -name "wp-config.php")" ]; then
-    wp config create \
-        --dbname=$MYSQL_DB_NAME \
-        --dbuser=$DB_USER_NAME \
-        --dbpass=$DB_USER_PWD \
-        --dbhost=$MYSQL_DB_NAME \
-        --locale=en_US \
-        --allow-root
-fi
-
 wp language core install en_US --activate --allow-root
 
 wp core install \
